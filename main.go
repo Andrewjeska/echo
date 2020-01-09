35dc4ca2b2dfd3e577265aeb549807e75b2a6989
Michael Anderjaska <michaela@blend.com>
2020-01-09 11:30:08 -0800

    revert and c-v

--- main.go
+++ main.go
@@ -72,4 +72,30 @@ func main() {
 	if err := graceful.Shutdown(app); err != nil {
 		logger.FatalExit(err)
 	}
+
+	ticker := time.NewTicker(3000 * time.Millisecond)
+	for {
+		select {
+		case <-ticker.C:
+			{
+
+				inStart := time.Now().UnixNano()
+				_, err := http.Get("https://echo.blend/status")
+				if err != nil {
+					log.Fatal(err)
+				}
+				inEnd := time.Now().UnixNano()
+
+				outStart := time.Now().UnixNano()
+				_, err = http.Get("https://echo.test.k8s.centrio.com/status")
+				if err != nil {
+					log.Fatal(err)
+				}
+				outEnd := time.Now().UnixNano()
+
+				log.Infof("\nhttps://echo.test.k8s.centrio.com/status: %d ms \nhttps://echo.blend/status: %d ms\n", (outEnd-outStart)/1000, (inEnd-inStart)/1000)
+
+			}
+		}
+	}
 }
